ff4dcd1e6db8142daba31bb6f5f71f760ac27e10
Christopher <christopher.ferraro34@gmail.com>
2024-06-07 15:28:31 -0400

    set max time feor preloader

--- src/components/Preloader.jsx
+++ src/components/Preloader.jsx
@@ -1,32 +1,33 @@
 import React, { useEffect } from 'react';
 
 export default function Preloader() {
-    useEffect(() => {
-        window.addEventListener('load', function() {
-          const loadingAnimation = document.querySelector('.lds-ellipsis');
-          const preloader = document.querySelector('.preloader');
-    
-          if (loadingAnimation) {
-            fadeOut(loadingAnimation);
-          }
-          if (preloader) {
-            setTimeout(() => {
-              fadeOut(preloader);
-            }, 333);
-          }
-        });
-      }, []);
-    
-      function fadeOut(element) {
-        element.style.opacity = 1;
-        (function fade() {
-          if ((element.style.opacity -= 0.1) < 0) {
-            element.style.display = 'none';
-          } else {
-            requestAnimationFrame(fade);
-          }
-        })();
+  useEffect(() => {
+    const preloader = document.querySelector('.preloader');
+
+    // Set maximum time for preloader (in milliseconds)
+    const maxPreloaderTime = 3000; // 3 seconds
+
+    // Fade out the preloader after the maximum time
+    setTimeout(() => {
+      fadeOut(preloader);
+    }, maxPreloaderTime);
+
+    // Event listener to fade out preloader when content has finished loading
+    window.addEventListener('load', () => {
+      fadeOut(preloader);
+    });
+  }, []);
+
+  function fadeOut(element) {
+    element.style.opacity = 1;
+    (function fade() {
+      if ((element.style.opacity -= 0.1) < 0) {
+        element.style.display = 'none';
+      } else {
+        requestAnimationFrame(fade);
       }
+    })();
+  }
     
       return (
         <div className="preloader">
