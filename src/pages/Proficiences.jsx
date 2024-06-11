e35ff97eefaabbf75c50d235d5b4a7d20a3ba40e
Christopher <christopher.ferraro34@gmail.com>
2024-06-11 19:56:54 -0400

    slider fixed to be the same across safari and chrome

--- src/pages/Proficiences.jsx
+++ src/pages/Proficiences.jsx
@@ -69,7 +69,7 @@ export default function Proficiences() {
           <div className="scroller wow fadeInUp" data-speed="fast">
             <div className="scroller-inner">
               {images.map((image, index) => (
-                <img key={index} className="scroll" src={image} alt={`Proficiency ${index + 1}`} />
+                <img key={index} className="scroll" src={image} alt={`Proficiency ${index + 1}`} width="100" height="100" />
               ))}
             </div>
           </div>
